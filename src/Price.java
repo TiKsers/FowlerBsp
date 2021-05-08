d2e64ead8e953f0d291e002864792ee601d187e4
Fabian Dittebrand <fabian.dittebrand@diva-e.com>
2021-05-08 14:21:44 +0200

    Replace Conditional with Polymorphism at getCharge() method.

--- src/Price.java
+++ src/Price.java
@@ -1,41 +1,47 @@
 public abstract class Price {
-    abstract int getPriceCode();
+    public abstract int getPriceCode();
 
+    public abstract double getCharge(int daysRented);
+}
+
+class ChildrensPrice extends Price {
+    @Override
+    public int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+
+    @Override
     public double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
         return result;
     }
 }
-class ChildrensPrice extends Price {
-    @Override
-    int getPriceCode() {
-        return Movie.CHILDRENS;
-    }
-}
+
 class NewReleasePrice extends Price {
     @Override
-    int getPriceCode() {
+    public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
+
 class RegularPrice extends Price {
     @Override
-    int getPriceCode() {
+    public int getPriceCode() {
         return Movie.REGULAR;
+    }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
     }
 }
